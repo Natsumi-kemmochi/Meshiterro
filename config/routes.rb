5e8a7ec9a3a6efa6105395cbe50b06c2262ec91e
Natsumi-kemmochi <172781785+Natsumi-kemmochi@users.noreply.github.com>
2024-08-06 14:04:08 +0000

    [finished]管理者ログイン機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
 
-  devise_for :admins
+  devise_for :admin, skip: [:registrations, :password], controllers: {
+    sessions: 'admin/sessions'
+  }
+
+
   devise_for :users
   root to: "homes#top"
   get 'homes/about' => 'homes#about', as: 'about'
+  
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
     resource :favorite, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]
